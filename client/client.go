28a6188d1d4aa04286af7aeed7ea960cc0e959ee
shiming <codingmylife@gmail.com>
2020-11-13 19:08:24 +0800

    fix: exported var populate issue

--- client/client.go
+++ client/client.go
@@ -11,13 +11,14 @@ import (
 	"time"
 )
 
-var(
+var (
 	EthClient *ethclient.Client
 )
 
 func Init() {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	EthClient, err := ethclient.DialContext(ctx, "https://mainnet.infura.io/v3/e9d43fcc8b60466c9b8c6c5b8215475c")
+	var err error
+	EthClient, err = ethclient.DialContext(ctx, "https://mainnet.infura.io/v3/e9d43fcc8b60466c9b8c6c5b8215475c")
 	defer cancel()
 	if err != nil {
 		log.Fatal(err)
@@ -30,26 +31,26 @@ func Init() {
 
 	tx, pending, _ := EthClient.TransactionByHash(ctx, common.HexToHash("0xbff5fa4aa3b503b9ae2b2b89332bb1cec736bac96c9eba30fb7f54522496a570"))
 	if !pending {
-		fmt.Println("tx cost: ",tx.Cost())
+		fmt.Println("tx cost: ", tx.Cost())
 	}
 }
 
-func ContractInvoke(){
+func ContractInvoke() {
 	address := common.HexToAddress("0x6f259637dcD74C767781E37Bc6133cd6A68aa161")
-	instance, err := hbtoken.NewHbtoken(address,EthClient)
+	instance, err := hbtoken.NewHbtoken(address, EthClient)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	name, err := instance.Name(&bind.CallOpts{})
-	if err != nil{
-		log.Fatal("get token name failed: " ,err.Error())
+	if err != nil {
+		log.Fatal("get token name failed: ", err.Error())
 	}
 	fmt.Println("token name: ", name)
 
-	balance, err := instance.BalanceOf(&bind.CallOpts{Pending: false,}, common.HexToAddress("0x46705dfff24256421a05d056c29e81bdc09723b8"))
-	if err != nil{
-		log.Fatal("contract interaction failed: ",err.Error())
+	balance, err := instance.BalanceOf(&bind.CallOpts{Pending: false}, common.HexToAddress("0x46705dfff24256421a05d056c29e81bdc09723b8"))
+	if err != nil {
+		log.Fatal("contract interaction failed: ", err.Error())
 	}
 	fmt.Println(balance)
 }
